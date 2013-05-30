c97770227f7d27430e0139b91b07b54c1ede057a
josh86 <jpadilla@neuone.com>
2013-05-30 16:59:07 -0500

    Almost done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Blogger::Application.routes.draw do
     resources :comments
   end
   
+  resources :tags
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
